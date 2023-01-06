f50310a6fb45029b99d2c8f8689581fa3bf5b0e5
Jiechenzheng <jiechen.zheng@outlook.com>
2023-01-06 17:46:37 +0800

    Implement StreamReassembler 1. Implement unreassembled buffer using deque 2. Implement when eof signal sent

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,96 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) :
+    _unreass_buffer(0),
+    _bitmap(0),
+    _first_unreass_index(0),
+    _unreass_cap(0),
+    _output(capacity),
+    _capacity(capacity),
+    _eof(false),
+    _last_byte_index(-1) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (eof)
+    {
+        _last_byte_index = index + data.length() - 1;
+        _eof = true;
+    }
+
+    /* update unreassemble buffer size according to reassembled byte stream */
+    _unreass_cap = _capacity - _output.buffer_size();
+
+    /* write to unreassemble buffer */
+    size_t len = data.length();
+    size_t end_string_index = min(index + len, _first_unreass_index + _unreass_cap);    // end index + 1
+    size_t ptr = _first_unreass_index;
+    while (ptr < end_string_index)
+    {
+        if (ptr < index)
+        {
+            if ((ptr - _first_unreass_index) >= _unreass_buffer.size()) // when no capacity in the queue
+            {
+                _unreass_buffer.push_back('\0');
+                _bitmap.push_back(false);
+            }
+            ptr++;
+        }
+        else {
+            if ((ptr - _first_unreass_index) >= _unreass_buffer.size())
+            {
+                _unreass_buffer.push_back(data[ptr - index]);
+                _bitmap.push_back(true);
+            }
+            else
+            {
+                _unreass_buffer[ptr - _first_unreass_index] = data[ptr - index];
+                _bitmap[ptr - _first_unreass_index] = true;
+            }
+            ptr++;
+        }
+    }
+
+    /* if there are contiguous bytes in the buffer, write the to byte stream */
+    check_contiguous();
+
+    return;
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+void StreamReassembler::check_contiguous(){
+    std::string tmp = "";
+    while (_bitmap.size() && _bitmap.front())
+    {
+        tmp += _unreass_buffer.front();
+        _unreass_buffer.pop_front();
+        _bitmap.pop_front();
+        _first_unreass_index++;
+    }
 
-bool StreamReassembler::empty() const { return {}; }
+    if (tmp.length())
+        _output.write(tmp);
+
+    /* if last byte is written to the bytestream, signal end_input */
+    if (_eof && _last_byte_index < static_cast<int>(_first_unreass_index))
+        _output.end_input();
+
+    return;
+}
+
+size_t StreamReassembler::unassembled_bytes() const {
+    size_t count = 0;
+
+    for (auto it = _bitmap.begin(); it != _bitmap.end(); it++)
+    {
+        if (*it) count++;
+    }
+
+    return count;
+}
+
+bool StreamReassembler::empty() const {
+    return _bitmap.size() ? false : true;
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -4,6 +4,7 @@
 #include "byte_stream.hh"
 
 #include <cstdint>
+#include <deque>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
@@ -11,9 +12,14 @@
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
+    std::deque<char> _unreass_buffer;
+    std::deque<bool> _bitmap;
+    size_t _first_unreass_index;
+    size_t _unreass_cap;
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    bool _eof;
+    int _last_byte_index;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -30,6 +36,8 @@ class StreamReassembler {
     //! \param index indicates the index (place in sequence) of the first byte in `data`
     //! \param eof the last byte of `data` will be the last byte in the entire stream
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
+
+    void check_contiguous();
 
     //! \name Access the reassembled byte stream
     //!@{
